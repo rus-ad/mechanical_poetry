dac979a085c034f6946d7518fd3575d95da7501c
Aleksander <rus91ad@mail.ru>
2022-05-09 21:39:56 +0700

    web part finished

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,9 +1,10 @@
-from app.api.handlers import handler, predict_handler
+from app.api.handlers import handler, predict_handler, finish_handler
 import os
 
 
 def setup_route(app):
     app.router.add_get('/', handler)
     app.router.add_post('/predict', predict_handler)
+    app.router.add_post('/finish', finish_handler)
     app.router.add_static("/static", os.path.join(os.getcwd(), 'templates/static'))
 
