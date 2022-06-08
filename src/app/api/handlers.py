bd08464435306413a2e1e16ddce34a5e4fb9f843
Aleksander <rus91ad@mail.ru>
2022-06-08 10:38:16 +0700

    add model

--- src/app/api/handlers.py
+++ src/app/api/handlers.py
@@ -1,6 +1,8 @@
 from aiohttp import web
 import aiohttp_jinja2
 
+from app.core.prompt import predict
+
 
 @aiohttp_jinja2.template('index.html')
 async def handler(requests: web.Request):
@@ -10,35 +12,14 @@ async def handler(requests: web.Request):
 @aiohttp_jinja2.template('finish.html')
 async def finish_handler(requests: web.Request):
     data = await requests.post()
-    print(data)
+    file = open('C:\\Users\\rusnak\\PycharmProjects\\mechanical_poetry\\src\\app\\stats.txt', 'a')
+    file.write(str(data))
+    file.close()
     return {}
 
 
 @aiohttp_jinja2.template('result.html')
 async def predict_handler(requests: web.Request):
-    data = await requests.post()
-    title = data['title']
-    poetry_type = data['type']
-    text = f"""
-    {poetry_type} {title} \n
-    I met a traveller from an antique land,
-    Who said—“Two vast and trunkless legs of stone
-    Stand in the desert. . . . Near them, on the sand,
-    Half sunk a shattered visage lies, whose frown,
-    And wrinkled lip, and sneer of cold command,
-    Tell that its sculptor well those passions read
-    Which yet survive, stamped on these lifeless things,
-    The hand that mocked them, and the heart that fed;
-    And on the pedestal, these words appear:
-    My name is Ozymandias, King of Kings;
-    Look on my Works, ye Mighty, and despair!
-    Nothing beside remains. Round the decay
-    Of that colossal Wreck, boundless and bare
-    The lone and level sands stretch far away.
-    """
+    text = await predict(requests)
     num_rows = str(len(text.split('\n')) + 5)
     return {'text': text, 'num_rows': num_rows}
-
-
-
-
--- src/app/service.py
+++ src/app/service.py
@@ -1,11 +1,12 @@
-from aiohttp import web
-import sys
 from app.routes import setup_route
 from app.utils.configurator import get_config
 from app.utils.logs import init_logger
-import os
+from app.core.prompt import load_poetry_generator
+
+import sys
 import jinja2
 import aiohttp_jinja2
+from aiohttp import web
 
 
 def init_app(argv):
@@ -13,6 +14,7 @@ def init_app(argv):
     app['config'] = get_config(argv)
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates/'))
     app['static_root_url'] = 'static/'
+    app['poetry_generator'] = load_poetry_generator()
     init_logger(app['config'])
     setup_route(app)
     return app
